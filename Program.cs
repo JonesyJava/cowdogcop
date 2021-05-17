93cd59971f540650c6faa40de5ecae4c3ea5911b
Jonesy <jonesyjava@gmail.com>
2021-05-17 12:02:03 -0600

    added game logic and ascii

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace cowdogcop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            int PlayerCount = 0;
+            int CompCount = 0;
+
+
+            Console.WriteLine(@"
+            
+                     /)  (\          
+                .-._((,~~.))_.-,       
+                 `-.   @@   ,-'         
+                   / ,o--o. \
+                  ( ( .__. ) )
+                   ) `----' (
+                  /          \ 
+                 /            \
+                /              \         
+                    /  \              
+                   /|oo \             
+                  (_|  /_)            
+                    `@/  \    _       
+                    |     \   \\      
+                     \||   \   ))     
+                     |||\ /  \//      
+                   _//|| _\   /       
+                  (_/(_|(____/  
+
+                   ,   /\   ,
+                  / '-'  '-' \
+                  |  POLICE  |
+                  |   .--.   |
+                  |  ( 21 )  |
+                  \   '--'   /
+                   '--.  .--'
+                       \/
+            ");
+            while (answer != "NO")
+            {
+                Console.WriteLine("Choose Wisely: \n->Cow\n->Dog\n->Cop");
+                string[] choices = new string[3] { "Cow", "Dog", "Cop" };
+                Random rndm = new Random();
+                int num = rndm.Next(0, 3);
+                Console.WriteLine("Make A Choice & Let FATE DECIDE: ");
+                string user = Console.ReadLine();
+                Console.WriteLine("Computer:" + choices[num]);
+
+                if (user == "Dog" && choices[num] == "Cop")
+                {
+                    Console.WriteLine("You LOSE! Cop TAZED Dog ... But he's ok and recovers! Good Puppy!");
+                    CompCount += 1;
+                }
+                else if (user == "Dog" && choices[num] == "Cow")
+                {
+                    Console.WriteLine("YOU WIN! Dog BITES Cow");
+                    PlayerCount += 1;
+                }
+                else if (user == "Cow" && choices[num] == "Dog")
+                {
+                    Console.WriteLine("You LOSE! Dog BITES Cow");
+                    CompCount += 1;
+                }
+                else if (user == "Cow" && choices[num] == "Cop")
+                {
+                    Console.WriteLine("You WIN! Cow KICKS Cop");
+                    PlayerCount += 1;
+                }
+                else if (user == "Cop" && choices[num] == "Cow")
+                {
+                    Console.WriteLine("You LOSE! Cow KICKS Cop");
+                    CompCount += 1;
+                }
+                else if (user == "Cop" && choices[num] == "Dog")
+                {
+                    Console.WriteLine("You WIN! Cop TAZED Dog! ... But he's ok and recovers! Good Puppy!");
+                    PlayerCount += 1;
+                }
+                else
+                {
+                    Console.WriteLine("TIE! ... INCREDIBLE ODDS!!");
+                }
+            }
+        }
+    }
+}
